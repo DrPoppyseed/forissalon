0c3e02f4f86abd254b24b150dcd2516ef3825fd7
Haruki Jay Shimada <peaske16180@gmail.com>
2020-05-28 16:32:49 +0900

    all shell files and some images

--- webpack.config.js
+++ webpack.config.js
@@ -4,86 +4,92 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const JavaScriptObfuscator = require('webpack-obfuscator');
 
 module.exports = {
-    entry: [
-        './src/js/index.js', 
-        './src/sass/style.scss', 
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
-    },
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: false,
-                extractComments: 'all',
-            }),
-            new OptimizeCSSAssetsPlugin({
-                cssProcessorOptions: {
-                    safe: true,
-                    discardComments: {
-                        removeAll: true
-                    }
-                }
-            })
-        ]
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html',
-            minify: {
-                collapseWhitespace: true
+  entry: [
+    './src/js/index.js',
+    './src/sass/style.scss',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false,
+        extractComments: 'all',
+      }),
+      new TerserPlugin(),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          safe: true,
+          discardComments: {
+            removeAll: true
+          }
+        }
+      })
+    ]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+      template: "./src/sass/style.scss"
+    }),
+    new CompressionPlugin({
+      test: /\.(js|css)/
+    }),
+    new UglifyJsPlugin({}),
+    new JavaScriptObfuscator ({
+      rotateUnicodeArray: true
+    }, ['./src/js/index.js'])
+  ],
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              name: '../dist/css/[name].css'
             }
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/style.css",
-            template: "./src/sass/style.scss"
-        }),
-        new CompressionPlugin({
-            test: /\.(js|css)/
-        }),
-        new UglifyJsPlugin({}),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '../dist/css/[name].css'
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader'
-                ]
-            }
+          },
+          {
+            loader: 'sass-loader'
+          }
         ]
-    }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
 };
-
